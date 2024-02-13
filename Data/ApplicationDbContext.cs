a7976a192637556407de25e17a4b598127a5167d
User Name <ST10025374@vcconnect.edu.za>
2024-02-13 16:57:31 +0200

    Update Category in Action Delete Category in Action

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,24 +1,33 @@
-﻿// Using directives to include necessary namespaces
-using BulkyWeb.Models; // Access to model classes, such as Category
+﻿using BulkyWeb.Models; // Access to model classes, such as Category
 using Microsoft.EntityFrameworkCore; // Access to EF Core functionality
 
-// Define the namespace for the current class, structuring the code within the BulkyWeb.Data area
 namespace BulkyWeb.Data
-{
+{  
     // Definition of ApplicationDbContext class that inherits from DbContext (EF Core's base class for working with a database)
     public class ApplicationDbContext : DbContext
     {
-        // Constructor that accepts database context options and passes them to the base DbContext class
+        ///---------------------------------------------------------------------------------------------///
+        /// <summary>
+        /// Constructor that accepts database context options and passes them to the base DbContext class
+        /// </summary>
+        /// <param name="options"></param>
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            // The constructor body is empty because the base class handles the options
+            
         }
 
-        // DbSet property for Categories, representing a table in the database
-        // This enables CRUD operations on Category entities
+        ///---------------------------------------------------------------------------------------------///
+        /// <summary>
+        /// DbSet property for Categories, representing a table in the database
+        /// This enables CRUD operations on Category entities
+        /// </summary>
         public DbSet<Category> Categories { get; set; }
 
-        // Overriding the OnModelCreating method to configure the model that EF Core uses to create the database
+        ///---------------------------------------------------------------------------------------------///
+        /// <summary>
+        /// Overriding the OnModelCreating method to configure the model that EF Core uses to create the database
+        /// </summary>
+        /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Configuring the Category entity
@@ -32,3 +41,4 @@ namespace BulkyWeb.Data
         }
     }
 }
+///-----------------------------------------------------------------< END >-----------------------------------------------------------------///
