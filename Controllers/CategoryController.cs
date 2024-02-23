3d89b98e6bc327795bc31bc844ad9603a23c5bcf
User Name <ST10025374@vcconnect.edu.za>
2024-02-23 17:56:33 +0200

    TempData and Partial Views

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,71 +1,102 @@
-﻿// Including necessary namespaces for the functionality required in this controller
-using BulkyWeb.Data; // Access to the application's DbContext (ApplicationDbContext)
-using BulkyWeb.Models; // Access to the application's model classes, such as Category
-using Microsoft.AspNetCore.Mvc; // Base namespace for MVC controller functionalities
+﻿using BulkyWeb.Data; 
+using BulkyWeb.Models; 
+using Microsoft.AspNetCore.Mvc; 
 
-// Define the namespace for the current class, structuring the code within the BulkyWeb.Controllers area
 namespace BulkyWeb.Controllers
 {
-    // CategoryController class inheriting from Controller, making it an MVC controller
     public class CategoryController : Controller
     {
-        // Field to hold the ApplicationDbContext instance
+        /// <summary>
+        /// Field to hold the ApplicationDbContext instance
+        /// </summary>
         private readonly ApplicationDbContext _db;
 
-        // Constructor that accepts an ApplicationDbContext to enable database operations within this controller
+        //---------------------------------------------------------------------//
+        /// <summary>
+        /// Constructor that accepts an ApplicationDbContext to enable database operations within this controller
+        /// Assigning the passed-in ApplicationDbContext to the private field
+        /// </summary>
+        /// <param name="db"></param>
         public CategoryController(ApplicationDbContext db)
         {
-            _db = db; // Assigning the passed-in ApplicationDbContext to the private field
+            _db = db;                                                   
         }
 
-        // Action method for handling requests to /Category/Index
-        // Returns a view displaying a list of categories
+        //---------------------------------------------------------------------//
+        /// <summary>
+        /// Action method for handling requests to /Category/Index
+        /// Returns a view displaying a list of categories
+        /// Retrieves all Category entities from the database and stores them in a list  
+        /// Returns the view associated with this action, passing the list of categories as a model
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
-        {
-            // Retrieves all Category entities from the database and stores them in a list
-            List<Category> objCategoryList = _db.Categories.ToList();
-            // Returns the view associated with this action, passing the list of categories as a model
-            return View(objCategoryList);
+        {        
+            List<Category> objCategoryList = _db.Categories.ToList();            
+            return View(objCategoryList);                               
         }
 
-        // GET action method for displaying the category creation form
+        //---------------------------------------------------------------------//
+        /// <summary>
+        /// GET action method for displaying the category creation form
+        /// Returns the view associated with the Create action
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Create()
         {
-            return View(); // Returns the view associated with the Create action
+            return View();
         }
 
-        // POST action method for handling the submission of the category creation form
-        [HttpPost]
+        //---------------------------------------------------------------------//
+        /// <summary>
+        /// POST action method for handling the submission of the category creation form
+        /// Custom validation: checks if the Category's Name matches its DisplayOrder
+        /// Adds a model error if the validation fails
+        /// Checks if the model state is valid after the custom validation
+        /// Adds the new Category to the DbContext and saves changes to the database
+        /// Commits the changes to the database
+        /// Adds a success message to TempData
+        /// Redirects to the Index action after successful creation
+        /// Returns the same view for corrections if the model state is invalid
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [HttpPost]                                                              
         public IActionResult Create(Category obj)
-        {
-            // Custom validation: checks if the Category's Name matches its DisplayOrder
-            if (obj.Name == obj.DisplayOrder.ToString())
+        {        
+            if (obj.Name == obj.DisplayOrder.ToString())// 
             {
-                // Adds a model error if the validation fails
                 ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name. ");
             }
 
-            // Checks if the model state is valid after the custom validation
             if (ModelState.IsValid)
             {
-                // Adds the new Category to the DbContext and saves changes to the database
                 _db.Categories.Add(obj);
-                _db.SaveChanges(); // Commits the changes to the database
-                return RedirectToAction("Index"); // Redirects to the Index action after successful creation
+                _db.SaveChanges(); 
+                TempData["success"] = "Category created successfully"; 
+                return RedirectToAction("Index"); 
             }
-            return View(); // Returns the same view for corrections if the model state is invalid
+            return View(); 
         }
 
-        // GET action method for displaying the category edit form for a specific category
+        //---------------------------------------------------------------------//
+        /// <summary>
+        /// GET action method for displaying the category edit form for a specific category
+        /// Validation to ensure an ID is provided
+        /// Returns a NotFound result if no ID is provided
+        /// Retrieves the Category entity by ID from the database
+        /// Returns a NotFound result if no category is found with the provided ID
+        /// Returns the view associated with this action, passing the found category as a model
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public IActionResult Edit(int? id)
         {
-            // Validation to ensure an ID is provided
             if (id == null || id == 0)
             {
-                return NotFound(); // Returns a NotFound result if no ID is provided
+                return NotFound(); 
             }
 
-            // Retrieves the Category entity by ID from the database
             Category? categoryFromDb = _db.Categories.Find(id);
 
             // Additional commented-out examples of retrieving a category entity
@@ -74,31 +105,40 @@ namespace BulkyWeb.Controllers
 
             if (categoryFromDb == null)
             {
-                return NotFound(); // Returns a NotFound result if no category is found with the provided ID
+                return NotFound(); 
             }
-            return View(categoryFromDb); // Returns the view associated with this action, passing the found category as a model
+            return View(categoryFromDb);  
         }
 
-        // POST action method for handling the submission of the category edit form
+        //---------------------------------------------------------------------//
+        /// <summary>
+        /// POST action method for handling the submission of the category edit form
+        /// Custom validation: checks if the Category's Name matches its DisplayOrder
+        /// Adds a model error if the validation fails
+        /// Checks if the model state is valid after the custom validation
+        /// Updates the existing Category in the DbContext and saves changes to the database
+        /// Commits the changes to the database
+        /// Redirects to the Index action after successful update
+        /// Returns the same view for corrections if the model state is invalid
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-            // Custom validation: checks if the Category's Name matches its DisplayOrder
             if (obj.Name == obj.DisplayOrder.ToString())
             {
-                // Adds a model error if the validation fails
                 ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name. ");
             }
 
-            // Checks if the model state is valid after the custom validation
             if (ModelState.IsValid)
             {
-                // Updates the existing Category in the DbContext and saves changes to the database
                 _db.Categories.Update(obj);
-                _db.SaveChanges(); // Commits the changes to the database
-                return RedirectToAction("Index"); // Redirects to the Index action after successful update
+                _db.SaveChanges(); 
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index"); 
             }
-            return View(); // Returns the same view for corrections if the model state is invalid
+            return View(); 
         }
 
         public IActionResult Delete(int? id)
@@ -127,7 +167,9 @@ namespace BulkyWeb.Controllers
 
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
     }
 }
+//**------------------------------------------------------------< END >------------------------------------------------------------**// 
